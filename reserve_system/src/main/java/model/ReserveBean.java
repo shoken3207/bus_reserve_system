8d11fbb6b535f61b46b2ba0472454f098bc91468
syunnn09 <syusuke5103@gmail.com>
2023-01-28 00:05:29 +0900

    [todo] JDBCドライバーが読み込めなくて詰んでいる...

--- reserve_system/src/main/java/model/ReserveBean.java
+++ reserve_system/src/main/java/model/ReserveBean.java
@@ -1,8 +1,16 @@
 package model;
 
 public class ReserveBean {
-	
+	private int id;
 	
 	public ReserveBean() { }
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 }
