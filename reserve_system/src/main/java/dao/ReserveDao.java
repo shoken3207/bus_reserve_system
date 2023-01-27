f738984963c359ac61e460b110ce5a9b6d185bd0
syusuke <syusuke5103@gmail.com>
2023-01-27 12:31:26 +0900

    [add] add ReserveDao

---
+++ reserve_system/src/main/java/dao/ReserveDao.java
@@ -0,0 +1,22 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.ReserveBean;
+
+public class ReserveDao {
+	private final String URL = "fukushima-pc";
+	private final String USER = "teamF";
+	private final String PASS = "figMySQL";
+
+	public List<ReserveBean> findAll() {
+		List<ReserveBean> list = new ArrayList<ReserveBean>();
+
+		try () {
+			
+		}
+
+		return list;
+	}
+}
