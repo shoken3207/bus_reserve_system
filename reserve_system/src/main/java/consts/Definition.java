d4459949776a7a54518bc81c8d6a71115f0ed1b6
syusuke <syusuke5103@gmail.com>
2023-02-14 12:31:27 +0900

    [remove] all

--- reserve_system/src/main/java/consts/Definition.java
+++
@@ -1,7 +0,0 @@
-package consts;
-
-public class Definition {
-	public static final String URL  = "jdbc:mysql://fukushima-pc/Ex_bus_System";
-	public static final String USER = "teamF";
-	public static final String PASS = "figMySQL";
-}
